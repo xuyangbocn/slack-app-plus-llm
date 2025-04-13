fc53deac2f17668d3c4af7ef6f2cc473ef3c4a0a
yangbo <xuyangbocn@gmail.com>
2025-04-13 16:14:16 +0800

    replace Agent class with minagent package

--- lambda_layer/py_openai_sdk/python/minagent/agent.py
+++ lambda_layer/py_openai_sdk/python/minagent/agent.py
@@ -1,26 +1,25 @@
 import logging
 import json
 from typing import Optional, Union, Any, List, Dict, Callable
+from typing_extensions import Literal
+
 import concurrent.futures as cf
 
-from openai import OpenAI, AzureOpenAI, AssistantEventHandler
+from openai import OpenAI, AzureOpenAI, AssistantEventHandler, NotGiven
 
 from .prompt import Prompt
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+NOT_GIVEN = NotGiven()
+
 
 class Agent(object):
     '''
-    Agent is a wrapper on top of any compatible interface of OpenAI chat completion API.
-    Agent is similar to beta OpenAI Assistant concept.
-    Each Agent has a pre-defined
-        - model
-        - system prompt
-        - tools
-        - other configurations
-    This can be used for non-OpenAI LLM, or when openai assistant api doesnt support certain features.
+    Agent is a wrapper on top of OpenAI chat completion and assistant API.
+
+    It handles `tool_call`, `file_search` within the class
     '''
 
     def __init__(
@@ -29,42 +28,119 @@ class Agent(object):
         name: str,
         description: str,
         model: str,
-        sentences: List[str],
-        tools: Union[List[object], None] = None,
-        tool_choice: Union[str, None] = 'auto',
+        instructions: List[str],
+
+        ##################
+        # Common Openai api parameters
+        ##################
+        metadata: Dict[str, str] | NotGiven = NOT_GIVEN,
+
+        reasoning_effort: Optional[  # for o-series only
+            Literal["low", "medium", "high"]] | NotGiven = NOT_GIVEN,
+        temperature: Optional[float] | NotGiven = NOT_GIVEN,
+        top_p: Optional[float] | NotGiven = NOT_GIVEN,
+        parallel_tool_calls: bool | NotGiven = NOT_GIVEN,
+        max_completion_tokens: Optional[int] | NotGiven = NOT_GIVEN,
+        tools: List[object] | NotGiven = NOT_GIVEN,
+        tool_choice: str | NotGiven = NOT_GIVEN,
+
+        # for assistant api only
+        tool_resources: Dict | None = None,
+
+        # for chat completion only
+        web_search_options: Dict | NotGiven = NOT_GIVEN,  # for limited models
+        frequency_penalty: Optional[float] | NotGiven = NOT_GIVEN,
+        presence_penalty: Optional[float] | NotGiven = NOT_GIVEN,
+        logit_bias: Optional[Dict[str, int]] | NotGiven = NOT_GIVEN,
+        logprobs: Optional[bool] | NotGiven = NOT_GIVEN,
+        top_logprobs: Optional[int] | NotGiven = NOT_GIVEN,
+        modalities: Optional[
+            List[Literal["text", "audio"]]] | NotGiven = NOT_GIVEN,
+
+        ##################
+        # Tool function for execution, Extra body, External data sources
+        ##################
         tool_functions: Dict[str, Callable] = {},
-        az_data_source: Dict = {},
+        extra_body: Dict | None = None,
+
         openai_asst_id: str = '',
     ):
         self.llm_client = llm_client
         self.name = name
         self.description = description
         self.model = model
-        self.prompt = Prompt(sentences)
-        self.tool_functions = tool_functions
+        self.prompt = Prompt(instructions)
+        self.tools = tools
+        self.tool_choice = tool_choice
+        self.tool_resources = tool_resources
+        self.parallel_tool_calls = parallel_tool_calls
+        self.reasoning_effort = reasoning_effort
+        self.temperature = temperature
+        self.top_p = top_p
+        self.max_completion_tokens = max_completion_tokens
+        self.metadata = metadata
+        self.web_search_options = web_search_options
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
+        self.logit_bias = logit_bias
+        self.logprobs = logprobs
+        self.top_logprobs = top_logprobs
+        self.modalities = modalities
 
-        # For chat completion API
-        self.chat_completion_kwargs = {'model': self.model}
-        if tools:
-            self.chat_completion_kwargs.update({
-                'tools': tools,
-                'tool_choice': tool_choice,
-            })
-        if (type(llm_client) is AzureOpenAI) and az_data_source:
-            self.chat_completion_kwargs.update({
-                'extra_body': {'data_sources': [az_data_source]}
-            })
-
-        # For Assistance API
-        self.asst_config = {
-            'name': name,
-            'description': description,
-            'instructions': self.prompt.instructions,
-            'model': model,
-            'tools': tools,
-        }
+        ##
         self.openai_asst = None
         self.openai_asst_id = openai_asst_id
+        self.tool_functions = tool_functions
+        self.extra_body = extra_body
+
+        self._set_chat_completion_kwargs()
+        self._set_asst_config()
+        self._set_asst_run_kwargs()
+
+    def _set_chat_completion_kwargs(self):
+        self.chat_completion_kwargs = {
+            'model': self.model,
+            'tools': self.tools,
+            'tool_choice': self.tool_choice,
+            'parallel_tool_calls': self.parallel_tool_calls,
+            'reasoning_effort': self.reasoning_effort,
+            'temperature': self.temperature,
+            'top_p': self.top_p,
+            'max_completion_tokens': self.max_completion_tokens,
+            'metadata': self.metadata,
+            'web_search_options': self.web_search_options,
+            'frequency_penalty': self.frequency_penalty,
+            'presence_penalty': self.presence_penalty,
+            'logit_bias': self.logit_bias,
+            'logprobs': self.logprobs,
+            'top_logprobs': self.top_logprobs,
+            'modalities': self.modalities,
+            'extra_body': self.extra_body,
+        }
+        return
+
+    def _set_asst_config(self):
+        self.asst_config = {
+            'name': self.name,
+            'description': self.description,
+            'instructions': self.prompt.instructions,
+            'model': self.model,
+            'tools': self.tools,
+            'tool_resources': self.tool_resources,
+            'reasoning_effort': self.reasoning_effort,
+            'temperature': self.temperature,
+            'top_p': self.top_p,
+            'metadata': self.metadata,
+        }
+        return
+
+    def _set_asst_run_kwargs(self):
+        self.asst_run_kwargs = {
+            'parallel_tool_calls': self.parallel_tool_calls,
+            'max_completion_tokens': self.max_completion_tokens,
+            'tool_choice': self.tool_choice,
+        }
+        return
 
     def _get_openai_asst(self):
         '''
@@ -99,6 +175,8 @@ class Agent(object):
         user: str,
         asst_thread_id: str,
         message: str,
+        additional_instructions: Optional[str] | NotGiven = NOT_GIVEN,
+        additional_messages: Optional[List[object]] | NotGiven = NOT_GIVEN,
     ) -> str:
         '''
         Send message to openai assistant api for response. Handles ordinary response and tool function calling
@@ -154,8 +232,22 @@ class Agent(object):
                             current_run_id,
                             tool_event.data.required_action.submit_tool_outputs.tool_calls
                         )
-
+                    elif tool_event.event == 'thread.run.failed':
+                        reply = "Failed to get response from assistant API"
+                        logger.error(reply)
+                        break
             return response
+
+        def handle_file_search_annotation(text_value, annotations):
+            # replace annotation in message text with actual file id
+            parsed_text = text_value
+            for a in annotations:
+                logger.info(f'File search annotation: {a.text}')
+                if hasattr(a, 'file_citation'):
+                    parsed_text = parsed_text.replace(
+                        a.text, f'[{a.file_citation.file_id}]')
+
+            return parsed_text
 
         if self.openai_asst is None:
             self.openai_asst = self._get_openai_asst()
@@ -167,18 +259,30 @@ class Agent(object):
         )
 
         reply = ''
+        annotations = []
 
         main_event_handler = AssistantEventHandler()
         with self.llm_client.beta.threads.runs.stream(
             thread_id=asst_thread_id,
             assistant_id=self.openai_asst.id,
             event_handler=main_event_handler,
+            additional_instructions=additional_instructions,
+            additional_messages=additional_messages,
+            **self.asst_run_kwargs,
         ) as stream:
             for event in stream:
                 logger.info(f'Stream event: {event.event}')
-                if event.event == 'thread.message.delta' and event.data.delta.content:
+                if event.data == 'error':
+                    reply = "LLM server had an error while processing your request"
+                    logger.error(reply)
+                    break
+                elif event.event == 'thread.message.delta' and event.data.delta.content:
                     # ordinary response from openai
                     reply += event.data.delta.content[0].text.value
+                    # annotations from file search
+                    if event.data.delta.content[0].text.annotations:
+                        annotations += event.data.delta.content[0].text.annotations
+
                 elif event.event == 'thread.run.requires_action':
                     # requires tool calls
                     reply = handle_require_actions(
@@ -186,13 +290,19 @@ class Agent(object):
                         main_event_handler.current_run.id,
                         event.data.required_action.submit_tool_outputs.tool_calls
                     )
+                elif event.event == 'thread.run.failed':
+                    reply = "Failed to get response from assistant API"
+                    logger.error(reply)
+                    break
+
+        reply = handle_file_search_annotation(reply, annotations)
 
         return reply
 
     def chat_completion(
         self,
         user: str,
-        messages: List[Dict[str, str]]
+        messages: List[Dict[str, str]],
     ) -> str:
         '''
         Send full thread of messages to openai chat completion api for response. Handles ordinary response and tool function calling
